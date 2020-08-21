8ff914be44b8537d75535c105cbd14cdaa405604
Gabriel Reis Miranda <gabriel.dev@golfarma.com.br>
2020-08-21 20:15:42 -0300

    Primeiro Commit - Projeto JAVA

---
+++ PBL - Compiladores/src/uefs/pbl/compiladores/view/App.java
@@ -0,0 +1,17 @@
+package uefs.pbl.compiladores.view;
+/**
+ * Classe Main da aplicação.
+ * @author Gabriel Reis Miranda
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
